2b6cad9f88b338c34b422f02c3dbd606963d069d
ismbyucjc025 <ismbuyucjc025@student.wethinkcode.co.za>
2025-12-16 11:36:50 +0200

    note component added

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,32 +1,21 @@
-import { useState } from 'react';
 
-function NoteForm() {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('New note:', { title, content });
-    setTitle('');
-    setContent('');
-  };
-
+function Note({note, onDelete, onArchive}) {
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <textarea
-        placeholder="Take a note..."
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <button type="submit">Add Note</button>
-    </form>
-  );
+    <div className = "note">
+      <h3>{note.title}</h3>
+      <p>{note.content}</p>
+      <div className="note-actions">
+        <button onClick={()=> onArchive(note.id)}>
+          {note.archived ? "Unarchive" : "Archive"}
+        </button>
+        <button onClick={() => onDelete(note.id)}>Delete</button>
+      </div>
+    </div>
+  )
+
 }
 
-export default NoteForm;
+
+
+export default Note;
---
+++ src/components/Noteform.js
@@ -0,0 +1,30 @@
+
+
+function NoteForm() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('New note:', { title, content });
+    setTitle('');
+    setContent('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Take a note..."
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button type="submit">Add Note</button>
+    </form>
+  );
+}
