2b31c6ca6bdefba1d5a262ca97d5673c0b97500c
ismbyucjc025 <ismbuyucjc025@student.wethinkcode.co.za>
2025-12-17 12:29:57 +0200

    added a addnote function to add new notes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import Header from 'components/Header';
+import NoteForm from 'components/NoteForm';
+import NoteList from 'components/NoteList';
+import { useState} from 'react';
 
 function  App() {
+    const [notes, setNotes] = useState([]);
+    
+    const addNote = (noteData) => {
+        const newNote = {
+            id: Date.now(),
+            ...noteData,
+            archived: false
+        };
+        setNotes([newNote, ...notes]);
+        
+    }
     return (
         <div className="App">
             <Header />
